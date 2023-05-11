2bc9ae19d2e85974ecb8d52895e0de801db49e5b
Shubhro Jyoti Hore <54320519+just-shubhro@users.noreply.github.com>
2023-05-11 20:32:31 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+    backend "azurerm" {
+    resource_group_name   = "rg-shubhro-ind"
+    storage_account_name  = "shubhro12"
+    container_name        = "tfstate"
+
+}
+  required_providers {
+    azurerm = {
+      # Specify what version of the provider we are going to utilise
+      source = "hashicorp/azurerm"
+      version = ">= 2.4.1"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+      key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+data "azurerm_client_config" "current" {}
+
+# Create our Virtual Network - Jonnychipz-VNET
+resource "azurerm_virtual_network" "vnet" {
+  name                = "jonnychipzvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+  resource_group_name = "rg-shubhro-ind"
+}
